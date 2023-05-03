09be5c1da9e807b1293cb124dc5bf012c9aec7df
Vvojciech <pakus94@gmail.com>
2023-05-03 22:48:45 +0200

    Added sample of calling api from frontend application

--- SeaBattleWeb/Controllers/HomeController.cs
+++ SeaBattleWeb/Controllers/HomeController.cs
@@ -2,12 +2,29 @@
 using Microsoft.AspNetCore.Mvc;
 using SeaBattleWeb.Models;
 using Microsoft.AspNetCore.Authorization;
+using Newtonsoft.Json;
 
 namespace SeaBattleWeb.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+
+    //[Authorize]
+    public async Task<IActionResult> PizzaTest()
+        {
+            //return View();
+            List<Pizza> pizzaList = new List<Pizza>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:5282/Pizza"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    pizzaList = JsonConvert.DeserializeObject<List<Pizza>>(apiResponse);
+                }
+            }
+            return View(pizzaList);
+        }
 
     public HomeController(ILogger<HomeController> logger)
     {
